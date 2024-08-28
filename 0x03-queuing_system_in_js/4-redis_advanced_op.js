b9c81108c4f1a2a5c4af4befd228eb7c334f9844
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-08-28 22:06:16 +0100

    Node Redis client and advanced operations

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -11,15 +11,14 @@ client.on('error', (err) => {
 // Promisify the hgetall function
 const hGetAllAsync = promisify(client.hgetall).bind(client);
 
-const hashValue = {
-  'Portland': '50',
-  'Seattle': '80',
-  'New York': '20',
-  'Bogota': '20',
-  'Cali': '40',
-  'Paris': '2'
-}
-
+const hashValue = [
+  'Portland', '50',
+  'Seattle', '80',
+  'New York', '20',
+  'Bogota', '20',
+  'Cali', '40',
+  'Paris', '2',
+];
 
 async function displayHashValue(hashName) {
   try {
@@ -32,6 +31,12 @@ async function displayHashValue(hashName) {
 
 client.on('ready', async () => {
   console.log('Redis client connected to the server');
-  await client.hset('HolbertonSchools', hashValue, print);
+  const promises = [];
+  for (let i = 0; i < hashValue.length; i += 2) {
+    promises.push(client.hset('HolbertonSchools', hashValue[i], hashValue[i + 1], print));
+  }
+
+  // Wait for all promises to resolve
+  await Promise.all(promises);
   await displayHashValue('HolbertonSchools');
 });
